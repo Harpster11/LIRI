0fd887aed781327d147b9b5efd800672c54286cd
John Harper <jharper1@tulane.edu>
2019-10-01 15:14:33 +0200

    Packages Installed, first command line request made for Bands in Town... Spotify Key Errors.... 404 errors w Bands In Town

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('Keys Loaded');
 
 exports.spotify = {
-    id: ProcessingInstruction.env.c0336da448544f4aa2de5f298c469e89,
-    secret: ProcessingInstruction.env.7b7fc68857e44629b256d07fdaa06024,
+    id: process.env.c0336da448544f4aa2de5f298c469e89,
+    // secret: ProcessingInstruction.env.7b7fc68857e44629b256d07fdaa06024,
 
 };
 
--- liri.js
+++ liri.js
@@ -4,4 +4,59 @@ require('dotenv').config();
 
 var keys = require("./keys.js");
 
-var spotify = new spotify(keys.spotify);
+// var spotify = new spotify(keys.spotify);
+// Was running into a problem - terminal says 'spotify is not a constructor'
+
+var axios = require("axios");
+
+
+// receive command from command line
+
+var command = process.argv[2];
+
+
+
+// search for concerts
+if (command == 'concert-this'){
+    console.log('Concert Search');
+    var concertArgs = process.argv;
+    var concert = "";
+    for (var i=2; i < concertArgs.length; i++){
+        if (i > 2 && i < concertArgs.length) {
+            concert = concert + "+" + concertArgs[i];
+          } else {
+            concert += concertArgs[i];
+    }
+console.log(concert);
+var concertQuery = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp";
+axios.get(concertQuery).then(
+    function(response) {
+      console.log("Bands Response: " + response.data);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
+}
+// play a song in spotify
+
+// search a movie
+
+// DO SOMETHING!
